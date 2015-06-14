c957b32fd0cb95dbde1b100e6b6a2726f4c0aa99
Phil Gyford <phil@gyford.com>
2015-06-14 11:49:20 +0100

    Update description

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     ],
     include_package_data=True,
     license='MIT License',
-    description='A Django app to copy stuff from your account on other services.',
+    description='A Django app to copy stuff from your accounts on other services.',
     long_description=README,
     url='https://github.com/philgyford/django-ditto',
     author='Phil Gyford',
