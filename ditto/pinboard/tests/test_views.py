dd30665e2a4bb1a125752f08af6b9974dfdf38c3
Phil Gyford <phil@gyford.com>
2015-06-18 18:29:17 +0100

    More Pinboard view tests

--- ditto/pinboard/tests/test_views.py
+++ ditto/pinboard/tests/test_views.py
@@ -7,11 +7,17 @@ from .. import factories
 class PinboardViewTests(TestCase):
 
     def test_home(self):
+        bookmarks = factories.BookmarkFactory.create_batch(10)
         response = self.client.get(reverse('pinboard'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'pinboard/bookmark_list.html')
         self.assertTemplateUsed(response, 'pinboard/base.html')
         self.assertTemplateUsed(response, 'ditto/ditto/base.html')
+        self.assertTrue('bookmark_list' in response.context)
+        self.assertEqual(
+            [bookmark.pk for bookmark in response.context['bookmark_list']],
+            [1,2,3,4,5,6,7,8,9,10]
+        )
 
     def test_bookmark_detail(self):
         bookmark = factories.BookmarkFactory.create()
@@ -19,4 +25,6 @@ class PinboardViewTests(TestCase):
                                                 kwargs={'pk': bookmark.pk}))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'pinboard/bookmark_detail.html')
+        self.assertTrue('bookmark' in response.context)
+        self.assertEqual(bookmark.pk, response.context['bookmark'].pk)
 
