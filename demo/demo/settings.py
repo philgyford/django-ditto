a1971cf871cae43938e20689f9969d3c0f890f4d
Phil Gyford <phil@gyford.com>
2015-07-23 16:53:58 +0100

    Turn djangodebugtoolbar on

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'ditto.ditto',
     'ditto.pinboard',
 )
