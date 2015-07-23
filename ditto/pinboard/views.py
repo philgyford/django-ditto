fbd8972d7c6c50320f942493133853a19524c24a
Phil Gyford <phil@gyford.com>
2015-07-23 19:19:43 +0100

    Display Pinboard bookmarks

--- ditto/pinboard/views.py
+++ ditto/pinboard/views.py
@@ -1,3 +1,5 @@
+from ..ditto.paginator import DiggPaginator
+
 from django.views.generic import DetailView, ListView
 
 from . import models
@@ -5,6 +7,9 @@ from . import models
 
 class HomeView(ListView):
     model = models.Bookmark
+    paginator_class = DiggPaginator
+    paginate_by = 50
+    page_kwarg = 'p'
 
 class BookmarkDetailView(DetailView):
     model = models.Bookmark
