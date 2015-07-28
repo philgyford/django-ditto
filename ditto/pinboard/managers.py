c26ede25d4d3451abd7f2f6c37ed20c026b56f6e
Phil Gyford <phil@gyford.com>
2015-07-28 18:07:05 +0100

    Add comment

--- ditto/pinboard/managers.py
+++ ditto/pinboard/managers.py
@@ -6,6 +6,10 @@ from taggit.managers import _TaggableManager
 class _BookmarkTaggableManager(_TaggableManager):
 
     def most_common_public(self):
+        """We want a way to list the most common tags but ignoring any private
+        Bookmarks. This is that way. An alternative to django-taggit's
+        standard `most_common()` method.
+        """
         extra_filters = {'bookmark__is_private': False}
 
         return self.get_queryset(extra_filters).annotate(
