d16c99dc9de102c17608d7438ec650408f266db5
Phil Gyford <phil@gyford.com>
2015-07-28 18:03:41 +0100

    Linebreak

--- ditto/pinboard/managers.py
+++ ditto/pinboard/managers.py
@@ -7,6 +7,7 @@ class _BookmarkTaggableManager(_TaggableManager):
 
     def most_common_public(self):
         extra_filters = {'bookmark__is_private': False}
+
         return self.get_queryset(extra_filters).annotate(
             num_times=models.Count(self.through.tag_relname())
         ).order_by('-num_times')
