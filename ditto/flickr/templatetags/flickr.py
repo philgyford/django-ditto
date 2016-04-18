a670a7827318185d2d33f9ba6ec11bf40356f67f
Phil Gyford <phil@gyford.com>
2016-04-18 19:33:16 +0100

    Add flickr template tags for recent/day photos.
    Fixes #106

--- ditto/flickr/templatetags/flickr.py
+++ ditto/flickr/templatetags/flickr.py
@@ -1,10 +1,54 @@
+import datetime
+import pytz
+
 from django import template
 
-from ..models import Photo
+from ..models import Photo, User
 from ...ditto.templatetags.ditto import display_time
 
 
 register = template.Library()
+
+
+@register.assignment_tag
+def recent_photos(nsid=None, limit=10):
+    """Returns a QuerySet of recent public Photos, in reverse-chronological
+    order.
+
+    Keyword arguments:
+    nsid -- A Flickr user's NSID. If not supplied, we fetch
+                    Photos for all Flickr users that have Accounts.
+    limit -- Maximum number to fetch. Default is 10.
+    """
+    if nsid is None:
+        users = User.objects_with_accounts.all()
+        photos = Photo.public_objects.filter(user=users)
+    else:
+        photos = Photo.public_objects.filter(user__nsid=nsid)
+    return photos.select_related()[:limit]
+
+@register.assignment_tag
+def day_photos(date, nsid=None):
+    """Returns a QuerySet of public Photos posted on a specific date.
+
+    Arguments:
+    date -- A date object.
+
+    Keyword arguments:
+    nsid -- A Flickr user's NSID. If not supplied, we fetch
+                    Photos for all Flickr users that have Accounts.
+    """
+    start = datetime.datetime.combine(date, datetime.time.min).replace(
+                                                            tzinfo=pytz.utc)
+    end   = datetime.datetime.combine(date, datetime.time.max).replace(
+                                                            tzinfo=pytz.utc)
+    photos = Photo.public_objects.filter(post_time__range=[start, end])
+    if nsid is None:
+        users = User.objects_with_accounts.all()
+        photos = photos.filter(user=users)
+    else:
+        photos = photos.filter(user__nsid=nsid)
+    return photos
 
 @register.simple_tag
 def taken_time(dt, granularity):
--- ditto/flickr/tests/test_templatetags.py
+++ ditto/flickr/tests/test_templatetags.py
@@ -1,3 +1,6 @@
+import datetime
+import pytz
+
 from django.test import TestCase
 
 from freezegun import freeze_time
@@ -5,6 +8,72 @@ from freezegun import freeze_time
 from ...ditto.templatetags.ditto import display_time
 from ...ditto.utils import datetime_now
 from ..templatetags import flickr
+from ..factories import AccountFactory, PhotoFactory, UserFactory
+
+
+class TemplatetagsRecentPhotosTestCase(TestCase):
+
+    def setUp(self):
+        user_1 = UserFactory(nsid='1234567890@N01')
+        user_2 = UserFactory(nsid='9876543210@N01')
+        account_1 = AccountFactory(user=user_1)
+        account_2 = AccountFactory(user=user_2)
+        self.photos_1 = PhotoFactory.create_batch(2, user=user_1)
+        self.photos_2 = PhotoFactory.create_batch(3, user=user_2)
+        self.private_photo = PhotoFactory(user=user_1, is_private=True)
+        # For comparing with the results:
+        self.public_pks = [p.pk for p in self.photos_1] + \
+                                                [p.pk for p in self.photos_2]
+
+    def test_recent_photos(self):
+        "Returns recent photos from all public accounts"
+        photos = flickr.recent_photos()
+        self.assertEqual(5, len(photos))
+        self.assertNotIn(self.private_photo.pk, self.public_pks)
+
+    def test_recent_photos_account(self):
+        "Returns recent photos from one account"
+        photos = flickr.recent_photos(nsid='1234567890@N01')
+        self.assertEqual(2, len(photos))
+
+    def test_recent_photos_limit(self):
+        photos = flickr.recent_photos(limit=4)
+        self.assertEqual(4, len(photos))
+
+
+class TemplatetagsDayPhotosTestCase(TestCase):
+
+    def setUp(self):
+        user_1 = UserFactory(nsid='1234567890@N01')
+        user_2 = UserFactory(nsid='9876543210@N01')
+        account_1 = AccountFactory(user=user_1)
+        account_2 = AccountFactory(user=user_2)
+        self.photos_1 = PhotoFactory.create_batch(2, user=user_1)
+        self.photos_2 = PhotoFactory.create_batch(3, user=user_2)
+
+        post_time = datetime.datetime(2015, 3, 18, 12, 0, 0).replace(
+                                                            tzinfo=pytz.utc)
+        self.photos_1[0].post_time = post_time
+        self.photos_1[0].save()
+        self.photos_2[1].post_time = post_time + datetime.timedelta(hours=1)
+        self.photos_2[1].save()
+
+        self.private_photo = PhotoFactory(user=user_1, is_private=True,
+                post_time = post_time)
+
+    def test_day_photos(self):
+        "Returns only public Photos from the date"
+        photos = flickr.day_photos(datetime.date(2015, 3, 18))
+        self.assertEqual(2, len(photos))
+        self.assertEqual(photos[0].pk, self.photos_2[1].pk)
+        self.assertEqual(photos[1].pk, self.photos_1[0].pk)
+
+    def test_day_photos_one_account(self):
+        "Returns only Photos from the day if it's the chosen account"
+        photos = flickr.day_photos(
+                            datetime.date(2015, 3, 18), nsid='1234567890@N01')
+        self.assertEqual(1, len(photos))
+        self.assertEqual(photos[0].pk, self.photos_1[0].pk)
 
 
 class TakenTimeTestCase(TestCase):
