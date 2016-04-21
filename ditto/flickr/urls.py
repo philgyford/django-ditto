ad7575345efeb9967830d567f37198fb30fc461c
Phil Gyford <phil@gyford.com>
2016-04-21 13:27:12 +0100

    Add slashes to the ends of urls

--- ditto/flickr/urls.py
+++ ditto/flickr/urls.py
@@ -9,27 +9,27 @@ urlpatterns = [
         name='index'
     ),
     url(
-        regex=r"^tags$",
+        regex=r"^tags/$",
         view=views.TagList.as_view(),
         name='tag_list'
     ),
     url(
-        regex=r"^tags/(?P<slug>[^/]+)$",
+        regex=r"^tags/(?P<slug>[^/]+)/$",
         view=views.TagDetail.as_view(),
         name='tag_detail'
     ),
     url(
-        regex=r"^(?P<nsid>[\dN@]+)$",
+        regex=r"^(?P<nsid>[\dN@]+)/$",
         view=views.UserDetail.as_view(),
         name='user_detail'
     ),
     url(
-        regex=r"^(?P<nsid>[\dN@]+)/tags/(?P<tag_slug>[^/]+)$",
+        regex=r"^(?P<nsid>[\dN@]+)/tags/(?P<tag_slug>[^/]+)/$",
         view=views.UserTagDetail.as_view(),
         name='user_tag_detail'
     ),
     url(
-        regex=r"^(?P<nsid>[\dN@]+)/(?P<flickr_id>\d+)$",
+        regex=r"^(?P<nsid>[\dN@]+)/(?P<flickr_id>\d+)/$",
         view=views.PhotoDetail.as_view(),
         name='photo_detail'
     ),
