f214de9a6510d1bc7796d8ec0fe9c7b22866085f
Phil Gyford <phil@gyford.com>
2017-08-18 18:24:02 +0100

    Change url_name context processor into templatetag

--- ditto/core/templatetags/ditto_core.py
+++ ditto/core/templatetags/ditto_core.py
@@ -122,3 +122,23 @@ def display_time(dt, link_to_day=False, granularity=0, case=None):
                 'visible': visible_time
             })
 
+
+@register.simple_tag(takes_context=True)
+def current_url_name(context):
+    """
+    Returns the name of the current URL, namespaced, or False.
+
+    Example usage:
+
+        {% current_url_name as url_name %}
+
+        <a href="#"{% if url_name == 'myapp:home' %} class="active"{% endif %}">Home</a>
+
+    """
+    url_name = False
+    if context.request.resolver_match:
+        url_name = "{}:{}".format(
+                                context.request.resolver_match.namespace,
+                                context.request.resolver_match.url_name
+                            )
+    return url_name
