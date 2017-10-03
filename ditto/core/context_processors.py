4c00345ec0a66363d3b9d7e423e2fcd19aa2a307
Phil Gyford <phil@gyford.com>
2017-10-03 12:16:54 +0100

    Remove unused 'DITTO_FLICKR_USE_LOCAL_MEDIA' in context_processor

--- ditto/core/context_processors.py
+++ ditto/core/context_processors.py
@@ -4,8 +4,6 @@ from ..flickr import app_settings
 
 def ditto(request):
     return {
-        'enabled_apps': ditto_apps.enabled(),
-        'DITTO_FLICKR_USE_LOCAL_MEDIA':
-                                    app_settings.DITTO_FLICKR_USE_LOCAL_MEDIA,
+        'enabled_apps': ditto_apps.enabled()
     }
 
