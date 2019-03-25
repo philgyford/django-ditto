231f70be5318f085263a80b67868d7b46e06d520
Phil Gyford <phil@gyford.com>
2019-03-25 10:46:22 +0000

    Fix comment

--- ditto/core/templatetags/ditto_core.py
+++ ditto/core/templatetags/ditto_core.py
@@ -26,7 +26,7 @@ def query_string(context, key, value):
     """
     For adding/replacing a key=value pair to the GET string for a URL.
 
-    eg, if we're viewing ?p=3 and we do {% url_replace order 'taken' %}
+    eg, if we're viewing ?p=3 and we do {% query_string order 'taken' %}
     then this returns "p=3&order=taken"
 
     And, if we're viewing ?p=3&order=uploaded and we do the same thing, we get
