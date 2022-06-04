2e943e8a63e77e2642b89063fd5ab40f05f6a854
Phil Gyford <phil@gyford.com>
2022-06-04 16:19:55 +0100

    Update version of black used for development

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ if sys.argv[-1] == "testpublish":
     # os.system("python setup.py bdist_wheel upload")
     sys.exit()
 
-dev_require = ["django-debug-toolbar>=2.0,<4.0", "flake8>=3.8,<5.0", "black==22.1.0"]
+dev_require = ["django-debug-toolbar>=2.0,<4.0", "flake8>=3.8,<5.0", "black==22.3.0"]
 tests_require = dev_require + [
     "factory-boy>=2.12.0,<4.0",
     "freezegun>=0.3.12,<2.0",
